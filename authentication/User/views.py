c1c09752c373fe19c6ccd89a8dedd2af00b78ae0
alexander784 <alexandernyaga1999@gmail.com>
2025-03-31 13:47:47 +0300

    Create LOgin user view

--- authentication/User/views.py
+++ authentication/User/views.py
@@ -1,8 +1,11 @@
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer,LoginSerializer,UserSerializer
 from rest_framework.response import Response
+from django.http import JsonResponse
+from rest_framework_simplejwt.tokens import RefreshToken
+
 
 # Create your views here.
 
@@ -17,3 +20,20 @@ def register_user(request):
         
         return Response(serializer.data, status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def login_user(request):
+    serializer = LoginSerializer(data=request.data)
+    if not serializer.is_valid():
+        return JsonResponse({'error': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+    user = serializer.validated_data['user']
+
+    refresh = RefreshToken.for_user(user)
+    return Response({
+        'refresh':str(refresh),
+        'access':str(refresh.access_token),
+        'user':UserSerializer(user).data
+    }, status=status.HTTP_200_OK)
--- authentication/authentication/settings.py
+++ authentication/authentication/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'User',
     'rest_framework',
+    'rest_framework_simplejwt.token_blacklist',
 
 
 ]
